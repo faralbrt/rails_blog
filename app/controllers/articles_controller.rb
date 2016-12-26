3bd4fe049c130908934378ba3079d030052505fe
faralbrt <albert.farhi5@gmail.com>
2016-12-26 16:29:24 -0500

    article model, show, controller and view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,21 @@
 class ArticlesController < ApplicationController
   def new
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+
+    @article.save
+    redirect "/articles/#{@article.id}"
+  end
+
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/articles/new' => 'articles#new'
   post '/articles' => 'articles#create'
+  get '/articles/:id' => 'articles#show'
 end
