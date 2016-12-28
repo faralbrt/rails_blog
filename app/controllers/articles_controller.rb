5ff2b5883240c25d8919e84299a46b4cee7e6d6a
faralbrt <albert.farhi5@gmail.com>
2016-12-27 19:40:06 -0500

    add validations and display errors

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,7 @@ class ArticlesController < ApplicationController
 
 
   def new
+    @article = Article.new
   end
 
   def show
@@ -15,8 +16,11 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
 
